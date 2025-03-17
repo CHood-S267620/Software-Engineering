79fe90c41b06225a39560ef1e5ea2ecc44b6c325
CHood-S267620 <S267620@uos.ac.uk>
2025-03-17 00:36:52 +0000

    changes

--- Website/Website/settings.py
+++ Website/Website/settings.py
@@ -117,10 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 import os 
-STATIC_URL = 'C:\Users\Cameron\Documents\Uni stuff\Year 2\Software Eng\Website\WebDev\static/'
+STATIC_URL = 'static/'
 STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIR = {
-    os.path.join(BASE_DIR, 'public/static')
+    os.path.join(BASE_DIR, 'static')
 }
 MEDIA_ROOT = os.path.join(BASE_DIR, 'public/static')
 MEDIA_URL = '/media/'
