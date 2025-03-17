4b9ce5f47cd67c1f087ce2efe4e7eb16249e418d
CHood-S267620 <S267620@uos.ac.uk>
2025-03-17 14:34:36 +0000

    changes

--- Website/Website/settings.py
+++ Website/Website/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 import os 
-STATIC_URL = '/Website/Website/WebDev/static/'
+STATIC_URL = '/../Website/WebDev/static/'
 STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIR = {
     os.path.join(BASE_DIR, 'public/static')
