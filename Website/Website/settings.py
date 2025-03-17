482e4f14f67f9967157d5d65ae6fa77d5b3dd6c0
CHood-S267620 <S267620@uos.ac.uk>
2025-03-17 00:52:18 +0000

    changes

--- Website/Website/settings.py
+++ Website/Website/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 import os 
 STATIC_URL = '/static/'
-STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT= "/Websit/WebDev/static/"
 STATICFILES_DIR = {
     os.path.join(BASE_DIR, 'static')
 }
